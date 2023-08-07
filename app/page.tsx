8c40bfb6151273f3d564cfde8f70e74a7ba76e6b
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-08-07 20:24:05 +0530

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <main className="flex flex-col items-center justify-between"></main>;
+  return (
+    <main className="flex flex-col items-center justify-between">
+      <div className="my-10 text-center ">
+        <h1 className="text-4xl font-bold">Palette Extractor</h1>
+        <p className="mt-3">Extract palettes from photos</p>
+      </div>
+      <div></div>
+    </main>
+  );
 }
