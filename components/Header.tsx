bac2708c541b5ce86a050eab7ed6f6f3e493e0fe
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-08-07 20:36:55 +0530

    add explore page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex items-center p-3 bg-light/50">
+    <header className="flex items-center px-4 py-3 bg-light/50">
       <Image
         src="/images/color-palette.png"
         width={30}
         height={30}
         alt="logo"
       />
-      <div className="ml-2 text-lg font-medium">GetPalette</div>
-    </div>
+      <div className="ml-2 text-lg font-semibold">GetPalette</div>
+      <div className="ml-auto">
+        <Link href="/community-palettes">Explore</Link>
+      </div>
+    </header>
   );
 };
 
