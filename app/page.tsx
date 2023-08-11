d9b8eadf6e7fb16df3599314863547ca9f781ec0
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-08-11 23:40:12 +0530

    added explore community palette link

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
 import CopyPalettes from "@/components/CopyPalettes";
 import ImagePaletteExtractor from "@/components/ImagePaletteExtractor";
 import AddIcon from "@/components/icons/AddIcon";
+import PaintingPaletteIcon from "@/components/icons/PaintingPaletteIcon";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between gap-10 p-6">
+    <main className="flex flex-col items-center justify-between gap-16 p-6">
       <div className="mt-10 text-center ">
         <h1 className="text-4xl font-bold">Palette Extractor</h1>
         <p className="mt-3">Generate palettes from images</p>
       </div>
       <ImagePaletteExtractor />
       <CopyPalettes />
-      <Button className="mb-10" disabled>
-        <AddIcon iconClass="w-5 h-5 mr-2" />
-        Add palette to community
-      </Button>
+      <div className="md:my-10 flex flex-col gap-6">
+        <Button className="">
+          <AddIcon iconClass="w-5 h-5 mr-2" />
+          Add palette to community
+        </Button>
+        <Link href="browse" className="text-center link">
+          <div className="p-1 text-lg font-medium text-darkest rounded">
+            <PaintingPaletteIcon iconClass="w-5 h-5 mr-2" />
+            <span className="">Explore community palettes</span>
+          </div>
+        </Link>
+      </div>
     </main>
   );
 }
