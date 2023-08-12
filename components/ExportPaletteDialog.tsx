eabd7b729ecb51bc016a831eeee669a7c1446cdc
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-08-12 15:42:53 +0530

    seperated css-export in own component

--- components/ExportPaletteDialog.tsx
+++ components/ExportPaletteDialog.tsx
@@ -21,13 +21,6 @@ import CheckCircleIcon from "./icons/CheckCircleIcon";
 
 const ExportPaletteDialog = () => {
   const palette = usePaletteStore((state) => state.palette);
-  const paletteName = usePaletteStore((state) => state.paletteName);
-  const colorNames = usePaletteStore((state) => state.colorNames);
-  let paletteCSS = "";
-  if (palette && paletteName && colorNames)
-    paletteCSS = generatePaleteCSS(palette, paletteName, colorNames);
-
-  const [copied, copy, resetCopied] = useCopy();
 
   return (
     <>
@@ -42,45 +35,61 @@ const ExportPaletteDialog = () => {
             <DialogTitle>Export Palette</DialogTitle>
           </DialogHeader>
           <div className="grid gap-4 p-4">
-            <Dialog>
-              <DialogTrigger>
-                <div className="w-20 h-20 p-4 flex flex-col items-center justify-center border border-mid/80 focus:bg-mid/20 hover:bg-mid/20 transition-colors rounded">
-                  <CSSIcon iconClass="" />
-                  CSS
-                </div>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>CSS</DialogHeader>
-                <div className="border border-mid/40 rounded overflow-hidden">
-                  <ScrollArea className="h-72">
-                    <HighlightedCSSCode paletteCSS={paletteCSS} />
-                  </ScrollArea>
-                </div>
-                <DialogFooter>
-                  <Button
-                    onClick={() => {
-                      copy(paletteCSS);
-                      resetCopied();
-                    }}
-                    className="w-40"
-                  >
-                    {copied ? (
-                      <>
-                        Copied! <CheckCircleIcon iconClass="ml-2 w-4 h-4" />
-                      </>
-                    ) : (
-                      <>
-                        Copy <CopyIcon iconClass="ml-2 w-4 h-4" />
-                      </>
-                    )}
-                  </Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
+            <CSSExport />
           </div>
         </DialogContent>
       </Dialog>
     </>
+  );
+};
+
+const CSSExport = () => {
+  const palette = usePaletteStore((state) => state.palette);
+
+  const paletteName = usePaletteStore((state) => state.paletteName);
+  const colorNames = usePaletteStore((state) => state.colorNames);
+  let paletteCSS = "";
+  if (palette && paletteName && colorNames)
+    paletteCSS = generatePaleteCSS(palette, paletteName, colorNames);
+
+  const [copied, copy, resetCopied] = useCopy();
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div className="w-20 h-20 p-4 flex flex-col items-center justify-center border border-mid/80 focus:bg-mid/20 hover:bg-mid/20 transition-colors rounded">
+          <CSSIcon iconClass="" />
+          CSS
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>CSS</DialogHeader>
+        <div className="border border-mid/40 rounded overflow-hidden">
+          <ScrollArea className="h-72">
+            <HighlightedCSSCode paletteCSS={paletteCSS} />
+          </ScrollArea>
+        </div>
+        <DialogFooter>
+          <Button
+            onClick={() => {
+              copy(paletteCSS);
+              resetCopied();
+            }}
+            className="w-40"
+          >
+            {copied ? (
+              <>
+                Copied! <CheckCircleIcon iconClass="ml-2 w-4 h-4" />
+              </>
+            ) : (
+              <>
+                Copy <CopyIcon iconClass="ml-2 w-4 h-4" />
+              </>
+            )}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
