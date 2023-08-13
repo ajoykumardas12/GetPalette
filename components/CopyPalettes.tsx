ed9a5c889b6e566273f411eaa5f878845a6083b6
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-08-13 20:52:25 +0530

    text-color in CopyPalette now accounts for dark and light bg

--- components/CopyPalettes.tsx
+++ components/CopyPalettes.tsx
@@ -3,7 +3,7 @@ import { Separator } from "./ui/separator";
 import CopyIcon from "./icons/CopyIcon";
 import { Button } from "./ui/button";
 import { usePaletteStore } from "@/store/paletteStore";
-import { rgbArrayToHex } from "@/lib/utils";
+import { isHexBgDark, rgbArrayToHex } from "@/lib/utils";
 import { useCopy } from "@/hooks/useCopy";
 import { Skeleton } from "./ui/skeleton";
 import CheckCircleIcon from "./icons/CheckCircleIcon";
@@ -31,6 +31,7 @@ const CopyPalettes = () => {
 const Color = ({ rgb }: { rgb: number[] }) => {
   const hex = rgbArrayToHex(rgb);
   const [copied, copy, resetCopied] = useCopy();
+  const isBgDark = isHexBgDark(hex.substring(1));
 
   return (
     <Button
@@ -44,13 +45,25 @@ const Color = ({ rgb }: { rgb: number[] }) => {
     >
       <div className="flex flex-grow items-center">
         {copied ? (
-          <CheckCircleIcon iconClass="w-7 h-7 stroke-brand" />
+          <CheckCircleIcon
+            iconClass={`w-7 h-7 ${
+              isBgDark ? "stroke-light/90" : "stroke-dark/90"
+            }`}
+          />
         ) : (
-          <CopyIcon iconClass="w-7 h-7 stroke-brand" />
+          <CopyIcon
+            iconClass={`w-7 h-7 ${
+              isBgDark ? "stroke-light/90" : "stroke-dark/90"
+            }`}
+          />
         )}
       </div>
-      <Separator className="mt-auto bg-stone-400/50" />
-      <div className="p-1 text-brand">{copied ? "copied!" : hex}</div>
+      <Separator
+        className={`mt-auto ${isBgDark ? "bg-light/20" : "bg-dark/20"}`}
+      />
+      <div className={`p-1 ${isBgDark ? "text-light/90" : "text-dark/90"}`}>
+        {copied ? "copied!" : hex}
+      </div>
     </Button>
   );
 };
