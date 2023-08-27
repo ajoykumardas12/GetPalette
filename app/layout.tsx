8139322ad073d12c2c9405209cd1c7312de65c6c
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-08-28 00:49:44 +0530

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,32 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Get Palette",
-  description: "Generate color palette from image",
+  title: "GetPalette",
+  description: "Generate beautiful color palettes from your images",
+  applicationName: "GetPalette",
+  authors: { url: "https://ajoykumardas.vercel.app/", name: "Ajoy Kumar Das" },
+  keywords: ["color palette", "color", "palette", "palette from image"],
+  robots: "index, follow",
+  icons:
+    "https://getpalette.vercel.app/_next/image?url=%2Fimages%2Fcolor-palette.png&w=32&q=75",
+  openGraph: {
+    type: "website",
+    url: "https://getpalette.vercel.app/",
+    title: "GetPalette",
+    description: "Generate beautiful color palettes from your images",
+    siteName: "GetPalette",
+    images: [
+      {
+        url: "https://getpalette.vercel.app/_next/image?url=%2Fimages%2Fnew-world.png&w=640&q=75",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@ajoykdas",
+    images:
+      "https://getpalette.vercel.app/_next/image?url=%2Fimages%2Fnew-world.png&w=640&q=75",
+  },
 };
 
 export default function RootLayout({
