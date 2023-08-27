873bc5beff7c3a17494ae4c7b39028d279189d08
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-08-27 19:41:17 +0530

    check if palette is already added to community palette

--- app/api/browse/route.ts
+++ app/api/browse/route.ts
@@ -6,6 +6,7 @@ const deta = Deta(process.env.DETA_PROJECT_KEY);
 const communityPalettesDB = deta.Base("community-palettes");
 
 export const GET = async (req: Request, res: Response) => {
+  // Returns all communityPalettes
   try {
     const communityPalettes = await communityPalettesDB.fetch();
     return NextResponse.json({
@@ -13,14 +14,18 @@ export const GET = async (req: Request, res: Response) => {
       data: communityPalettes,
     });
   } catch (error) {
+    // Failed to fetch
     return NextResponse.json({ status: 400, error: error });
   }
 };
 
 export const POST = async (req: Request, res: Response) => {
+  // Validates palette data and adds to db unless already exists
   const body = await req.json();
 
+  // If request has body, proceed
   if (body) {
+    // Check if body contains correct palette data
     if (isCommunityPalette(body)) {
       const data = {
         name: body.name,
@@ -29,15 +34,31 @@ export const POST = async (req: Request, res: Response) => {
       };
 
       try {
-        const palette = await communityPalettesDB.insert(data, data.slug);
-        return NextResponse.json({ status: 200, palette: palette });
+        // Check if palette already added
+        const alreadyAdded = await communityPalettesDB.get(data.slug);
+        if (alreadyAdded) {
+          return NextResponse.json({
+            status: 409,
+            error: "This palette is already added!",
+          });
+        } else {
+          // Palette isn't already added, add
+          const palette = await communityPalettesDB.put(data, data.slug);
+          return NextResponse.json({ status: 200, palette: palette });
+        }
       } catch (error) {
-        return NextResponse.json({ status: 400, error: error });
+        // Return error
+        return NextResponse.json({
+          status: 400,
+          error: "Something went wrong! Please try again.",
+        });
       }
     } else {
+      // Body doesn't contain correct palette data
       return NextResponse.json({ status: 500, error: "Invalid palette data." });
     }
   } else {
+    // request doesn't have body
     return NextResponse.json({ status: 401, error: "Missing palette data" });
   }
 };
